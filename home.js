3c3ac8f2c491a5f012fb1ae3bab634ce3d18da2f
Asem <ismailovaasem363@gmail.com>
2025-01-21 14:47:03 +0500

    homework added

---
+++ home.js
@@ -0,0 +1,13 @@
+function check (mainDish, sideDish, dessert, drink, saladOrSnack, chaevue) {
+
+  let bill = mainDish + sideDish + dessert + drink + saladOrSnack;
+  
+  let total =  (bill * chaevue) + bill;
+  let totalBill = total.toFixed(2);
+  
+  console.log('Ваш общий счет с чаевыми составляет $' + totalBill);
+  }
+  
+  check(15, 10, 6, 4, 5, 0.15);
+  check(115, 110, 16, 14, 15, 0.20);
+  check(1005, 910, 67, 41, 55, 0.25);
